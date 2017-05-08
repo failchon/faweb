8dbf73320c54ae4a4934612ad05a8da1d3365a28
failchon <kill.to@live.com>
2017-05-08 10:21:36 +0300

    dd
    dd

--- src/app/components/pages/discord/discord.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'discord',
-  templateUrl: 'discord.component.html'
-})
-export class DiscordComponent  { 
- }
--- src/app/components/pages/raids/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './raids.component';
-export * from './introduction/introduction.component';
-export * from './encounters/encounter.component';
