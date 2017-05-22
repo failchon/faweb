881a4841a8abc0a39cae2de7f5a903bf90c23ae0
failchon <kill.to@live.com>
2017-05-22 16:53:49 +0300

    style changes on calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,6 @@ import { isSameMonth, isSameDay, startOfMonth, endOfMonth, startOfWeek, endOfWee
 import { CalendarEvent, CalendarEventTimesChangedEvent } from 'angular-calendar';
 import { colors } from '../demo-utils/colors';
 
-interface Film {
-  id: number;
-  title: string;
-  release_date: string;
-}
-
-interface FilmEvent extends CalendarEvent {
-  film: Film;
-}
-
 @Component({
   selector: 'my-app',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,68 +15,4 @@ interface FilmEvent extends CalendarEvent {
 })
 
 export class AppComponent  { 
-title ="Fearless Alliance";
- view: string = 'week';
- //view: string = 'month';
- //view: string = 'day';
-
-  viewDate: Date = new Date();
-
-  events$: Observable<FilmEvent[]>;
-
-  activeDayIsOpen: boolean = false;
-
-  constructor(private http: Http) {}
-
-  ngOnInit(): void {
-    this.fetchEvents();
-  }
-
-  fetchEvents(): void {
-
-    const getStart: any = {
-      month: startOfMonth,
-      week: startOfWeek,
-      day: startOfDay
-    }[this.view];
-
-    const getEnd: any = {
-      month: endOfMonth,
-      week: endOfWeek,
-      day: endOfDay
-    }[this.view];
-
-    const search: URLSearchParams = new URLSearchParams();
-    search.set('primary_release_date.gte', format(getStart(this.viewDate), 'YYYY-MM-DD'));
-    search.set('primary_release_date.lte', format(getEnd(this.viewDate), 'YYYY-MM-DD'));
-    search.set('api_key', '0ec33936a68018857d727958dca1424f');
-    this.events$ = this.http
-      .get('assets/json/birthday-data.json', {search})
-      .map(res => res.json())
-      .map(({results}: {results: Film[]}) => {
-        return results.map((film: Film) => {
-          return {
-            title: film.title,
-            start: new Date(film.release_date),
-            color: colors.yellow,
-            film
-          };
-        });
-      });
-  }
-
-  dayClicked({date, events}: {date: Date, events: CalendarEvent[]}): void {
-
-    if (isSameMonth(date, this.viewDate)) {
-      if (
-        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
-        events.length === 0
-      ) {
-        this.activeDayIsOpen = false;
-      } else {
-        this.activeDayIsOpen = true;
-        this.viewDate = date;
-      }
-    }
-  }
 }
--- src/app/components/pages/home/events/events.component.ts
+++ src/app/components/pages/home/events/events.component.ts
@@ -1,26 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { HomeService } from '../../../../services/index';
 import { Observable } from 'rxjs';
-import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Http, URLSearchParams } from '@angular/http';
+import { Subject } from 'rxjs/Subject';
+import { addDays } from 'date-fns';
+import { isSameMonth, isSameDay, startOfMonth, endOfMonth, startOfWeek, endOfWeek, startOfDay, endOfDay, format } from 'date-fns';
+import { CalendarEvent, CalendarEventTimesChangedEvent } from 'angular-calendar';
+import { colors } from 'demo-utils/colors';
+
+interface Film {
+  id: number;
+  title: string;
+  release_date: string;
+}
+
+interface FilmEvent extends CalendarEvent {
+  film: Film;
+}
 
 @Component({
   selector: 'events',
+	changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'events.component.html'
 })
 export class EventsComponent  {
-	 h = new Date(2017,4,21);	 
-	 missions = new Date(2017,4,21);	
-	 firstwing = this.missions.setDate(this.missions.getDate() + 1);
-	 secondwing = this.missions.setDate(this.missions.getDate() + 4);
-	 thirdwing = this.missions.setDate(this.missions.getDate() + 2);
-	 fourthwing = this.missions.setDate(this.missions.getDate() + 1);
+view: string = 'week';
+ //view: string = 'month';
+ //view: string = 'day';
 
-	
+  viewDate: Date = new Date();
 
-	 tab = 0;
+  events$: Observable<FilmEvent[]>;
 
-  setTab(num: number) {
-    this.tab = num;
+  activeDayIsOpen: boolean = false;
+
+  constructor(private http: Http) {}
+
+  ngOnInit(): void {
+    this.fetchEvents();
+  }
+
+  fetchEvents(): void {
+
+    const getStart: any = {
+      month: startOfMonth,
+      week: startOfWeek,
+      day: startOfDay
+    }[this.view];
+
+    const getEnd: any = {
+      month: endOfMonth,
+      week: endOfWeek,
+      day: endOfDay
+    }[this.view];
+
+    const search: URLSearchParams = new URLSearchParams();
+    search.set('primary_release_date.gte', format(getStart(this.viewDate), 'YYYY-MM-DD'));
+    search.set('primary_release_date.lte', format(getEnd(this.viewDate), 'YYYY-MM-DD'));
+    search.set('api_key', '0ec33936a68018857d727958dca1424f');
+    this.events$ = this.http
+      .get('assets/json/birthday-data.json', {search})
+      .map(res => res.json())
+      .map(({results}: {results: Film[]}) => {
+        return results.map((film: Film) => {
+          return {
+            title: film.title,
+            start: new Date(film.release_date),
+            color: colors.yellow,
+            film
+          };
+        });
+      });
+  }
+
+  dayClicked({date, events}: {date: Date, events: CalendarEvent[]}): void {
+
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
   }
 
 }
