36c5cd6f62a97b8d66587d9d049089fefde0e41f
failchon <kill.to@live.com>
2017-06-06 11:44:25 +0300

    trying to add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,29 @@ import { addDays } from 'date-fns';
 import { isSameMonth, isSameDay, startOfMonth, endOfMonth, startOfWeek, endOfWeek, startOfDay, endOfDay, format } from 'date-fns';
 import { CalendarEvent, CalendarEventTimesChangedEvent } from 'angular-calendar';
 import { colors } from '../demo-utils/colors';
+import { Router, NavigationEnd} from "@angular/router";
+
+declare var ga:Function;
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
 })
 
-export class AppComponent  { 
+export class AppComponent  {
+
+  private currentRoute:string;
+
+    constructor(public router: Router) {
+            router.events.distinctUntilChanged((previous: any, current: any) => {
+                if(current instanceof NavigationEnd) {
+                    return previous.url === current.url;
+                }
+                return true;
+            }).subscribe((x: any) => {
+                console.log('router.change', x);
+                ga('send', 'pageview', x.url);
+            });
+        }
+  
 }
