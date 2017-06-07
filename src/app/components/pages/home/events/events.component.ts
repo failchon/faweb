c203abc280bc9421f219c73732ed836151e6ec41
failchon <kill.to@live.com>
2017-06-07 14:39:24 +0300

    added some things to calendar

--- src/app/components/pages/home/events/events.component.ts
+++ src/app/components/pages/home/events/events.component.ts
@@ -27,13 +27,14 @@ interface FaeEvent extends CalendarEvent {
   templateUrl: 'events.component.html'
 })
 export class EventsComponent  {
- view: string = 'month';
+  view: string = 'month';
  //view: string = 'month';
  //view: string = 'day';
 
+
   viewDate: Date = new Date();
 
-  events$: Observable<FaeEvent[]>;
+  events$: Observable<CalendarEvent[]>;
 
   activeDayIsOpen: boolean = false;
 
@@ -98,5 +99,10 @@ export class EventsComponent  {
     }
   }
 
+
+  eventClicked(event: Fae): void {
+
+    window.open("https://docs.google.com/spreadsheets/d/152ndLffYQCB5AesRzUGkeElrBR5m7QibPpVGHqK9Wj8/edit#gid=1274985437" + event.title);
 }
 
+}
