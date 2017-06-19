7ff31fb67863ed00c26ac67a58dee41c1eb9bd80
failchon <kill.to@live.com>
2017-06-19 13:01:39 +0300

    dd

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
