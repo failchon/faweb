d34809f6936ac050a67fc9d095cf67b4d5661873
failchon <kill.to@live.com>
2017-06-27 09:01:16 +0300

    added 26.06.2017 raid clear

--- src/app/components/pages/raids/index.ts
+++ src/app/components/pages/raids/index.ts
@@ -5,6 +5,7 @@ export * from './raids-hover/guild/raidguild.component';
 export * from './raids-hover/guild/tables/05.06.2017/05_06_2017.component';
 export * from './raids-hover/guild/tables/12.06.2017/12_06_2017.component';
 export * from './raids-hover/guild/tables/19.06.2017/19_06_2017.component';
+export * from './raids-hover/guild/tables/26.06.2017/26_06_2017.component';
 export * from './raid-guides/vg/vg.component';
 export * from './raid-guides/gorseval/gorseval.component';
 export * from './raid-guides/sabetha/sabetha.component';
