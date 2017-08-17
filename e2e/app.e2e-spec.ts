4c73ae4936a0a9c5142dc5baaa202e7903dbf650
Ovidiu Unguru <ovidiu.unguru@softescu.ro>
2017-08-17 10:41:16 +0300

    trying to update angular cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { FaPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('fa App', () => {
-  let page: FaPage;
+describe('faweb2 App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new FaPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
